8031432a5f03318c383b4e5071c10abdad4573e9
richrosenthal <rrosenthal2013@gmail.com>
2020-10-14 21:23:46 -0400

    adds associations

--- backend/PM_API/db/migrate/20201015010540_create_tasks.rb
+++ backend/PM_API/db/migrate/20201015010540_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :description
       t.date :due_date
-
+      t.belongs_to :equipment
       t.timestamps
     end
   end
