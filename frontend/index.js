0f7da9c38d118a1c326dffb033b6ea46ee35054d
richrosenthal <rrosenthal2013@gmail.com>
2020-10-14 21:56:10 -0400

    create skeleton fetch request and base url

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  debugger;
+
+
 })
+
+const BASE_URL = "http://localhost:3000"
+ // read - fetch users index
+
+ function fetchUsers(){
+    fetch()
+    .then()
+    .then()
+ }
+
+ // create - create a new user
+
+ // delete - delete a user
+
+ // create a piece of equipment
+
+ // edit a piece of equipment
+
+ // delete a piece of equipment
+
+ // create a task
+
+ // delete a task
